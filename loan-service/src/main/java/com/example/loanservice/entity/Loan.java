a9471ae902be942d42056437a16e37ada18eda49
GabrielaSoruco <soruco.gabi@gmail.com>
2022-06-25 18:45:06 -0300

    metodo saveLoan terminado config server no conectado

--- loan-service/src/main/java/com/example/loanservice/entity/Loan.java
+++ loan-service/src/main/java/com/example/loanservice/entity/Loan.java
@@ -1,12 +1,16 @@
 package com.example.loanservice.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "loan")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Loan {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/patagoniatest/feignclients/LoanFeignClient.java
+++ src/main/java/com/example/patagoniatest/feignclients/LoanFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "loan-service", url = "http://localhost:8082")
+@FeignClient(name = "loan-service", url = "http://localhost:8080")
 @RequestMapping("/loan")
 public interface LoanFeignClient {
 
